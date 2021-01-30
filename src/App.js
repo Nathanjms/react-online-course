d352c97537c175b16b0ce45bb73e84a820550add
Nathan James <nathanjms@outlook.com>
2021-01-30 23:23:25 +0000

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function SecretComponent() {
+  return <h1>Secret info for authorised users only</h1>
 }
+
+function RegularComponent() {
+  return <h1>Everyone can see this component.</h1>
+}
+function App(props) {
+  return (
+    <>
+      {props.authorised ? <SecretComponent /> : <RegularComponent />}
+    </>
+  )
+
+}
+
+
 
 export default App;
