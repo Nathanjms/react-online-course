c795a032be8e9af123d2115722863824a0ce2fe9
Nathan James <nathanjms@outlook.com>
2021-01-31 16:18:39 +0000

    GitHub api call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-function SecretComponent() {
-  return <h1>Secret info for authorised users only</h1>
-}
+// https://api.github.com/users/nathanjms
 
-function RegularComponent() {
-  return <h1>Everyone can see this component.</h1>
-}
-function App(props) {
-  return (
-    <>
-      {props.authorised ? <SecretComponent /> : <RegularComponent />}
-    </>
-  )
+function App({ login }) {
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then(response => response.json())
+      .then(setData)
+      .then(setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if (!data) return null;
+
+  return (<div>
+    <h1>{data.name}</h1>
+    <p>{data.location}</p>
+    <img alt={data.login} src={data.avatar_url} />
+  </div>)
+
 
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App"
 
-const [, , light] = ["boots", "tent", "headlamp"];
-console.log(light);
-
 ReactDOM.render(
-  <App authorised={false} />,
+  <App login="nathanjms" />,
   document.getElementById('root')
 );
 
