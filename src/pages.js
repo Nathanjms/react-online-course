d7e50cbef8bc844d9fbe62fbb0eec45904ef6f03
Nathan James <nathanjms@outlook.com>
2021-02-06 12:36:57 +0000

    Error 404 function

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
-import React from "react"
+import React from "react";
+import { Link, useLocation } from "react-router-dom"
 
 export function Home() {
     return (
         <div>
             <h1>[Company Website]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="events">Events</Link>
+                <Link to="contact">Contact</Link>
+            </nav>
         </div>
     );
 }
@@ -30,4 +36,12 @@ export function Contact() {
             <h1>[Contact]</h1>
         </div>
     );
+}
+
+export function Whoops404() {
+    return (
+        <div>
+            <h1>Resource not found at {useLocation().pathname}</h1>
+        </div>
+    )
 }
